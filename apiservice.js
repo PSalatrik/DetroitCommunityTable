15fb91bb017c0f3749021d1a6197c25b81bc9996
TimHeaphy <theheaphy@gmail.com>
2015-12-08 15:54:10 -0500

    yelpAPI index oauth script and corktown partial

--- apiservice.js
+++ apiservice.js
@@ -4,40 +4,40 @@
                 return result;
             }
 
-var app = angular.module("dcTable", [])
+var app = angular.module("dcTable");
 
-app.controller('yelpity', ['$scope', 'yelpService', function($scope, yelpService) {
+app.controller('yelpity', ['$scope', 'myYelpAPI', function($scope, myYelpAPI) {
     $scope.businesses = [];
-    MyYelpAPI.retrieveYelp('', function(data) {
+    myYelpAPI.retrieveYelp('', function(data) {
         $scope.businesses = data.businesses[0].name;
         console.log(data);
     });
 
-}]).service('yelpService', function($http){
-
-   return {
-       var method: 'GET',
-       var url: "https://api.yelp.com/v2/business/yelp-detroit",
-      
-       var para = {
-        oauth_consumer_key: NuLE3hAVaJWmVMyRMnTM-A,
-        oauth_token: WzPzrA7tcIEv-YuRR1_ilXdyylqOdvVH,
-        oauth_signature_method: "HMAC-SHA1",
-        oauth_timestamp: new Date().getTime(),
-        oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-
+}]).factory("myYelpAPI", function($http) {
+    return {
+        "retrieveYelp": function(name, callback) {
+            var method = 'GET';
+            var url = 'http://api.yelp.com/v2/search';
+            var params = {
+                    callback: 'angular.callbacks._0',
+                    location: 'Detroit',
+                    oauth_consumer_key: 'NuLE3hAVaJWmVMyRMnTM-A', //Consumer Key
+                    oauth_token: 'WzPzrA7tcIEv-YuRR1_ilXdyylqOdvVH', //Token
+                    oauth_signature_method: "HMAC-SHA1",
+                    oauth_timestamp: new Date().getTime(),
+                    oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+                    term: 'food'
+                };
+            var consumerSecret = 'adjPf1c0JJy2LWeAsqEz7QrL_sQ'; //Consumer Secret
+            var tokenSecret = '8gyS5zdHd4uaVCPiEoxZu_PjOU4'; //Token Secret
+            var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
+            params['oauth_signature'] = signature;
+            $http.jsonp(url, {params: params}).success(callback);
+        }
     }
-
-      var consumer_secret= adjPf1c0JJy2LWeAsqEz7QrL_sQ
-      var token_secret = 8gyS5zdHd4uaVCPiEoxZu_PjOU4
-      var signature = oauthSignature.generate(method, url, param, consumerSecret, tokenSecret, { encodeSignature: false});
-                        params['oauth_signature'] = signature;
-          //$http.jsonp(url, {params: params}).success(callback);
-    }).then(function workingCallback(response){
-      displayFoodStuff.data = response.data.;
-    }, function errorCallBack(response){
-      console.log("Stop Yelping at me")
-    });
+});
       
 
-})
+
+
+ 
