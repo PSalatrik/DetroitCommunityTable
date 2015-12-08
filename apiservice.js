15ef2b1b0a998a4c2cb8fb79512a9d282b391deb
PSalatrik <philip.salatrik@gmail.com>
2015-12-08 13:43:55 -0500

    apiUpdate

--- apiservice.js
+++ apiservice.js
@@ -1,26 +1,37 @@
+ function randomString(length, chars) {
+                var result = '';
+                for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+                return result;
+            }
 
-var app = angular.module("dcTable")
+var app = angular.module("dcTable", [])
 
 app.service('yelpService', function($http){
-    var displayFoodStuff = {};
 
-    $http({
-      method: 'GET',
-      url: yelp.com
-      Consumer Key  NuLE3hAVaJWmVMyRMnTM-A
-      Consumer Secret adjPf1c0JJy2LWeAsqEz7QrL_sQ
-      Token WzPzrA7tcIEv-YuRR1_ilXdyylqOdvVH
-      Token Secret  8gyS5zdHd4uaVCPiEoxZu_PjOU4
+   return $http({
+       var method: 'GET',
+       var url: "https://api.yelp.com/v2/business/yelp-detroit",
+      
+       var para ={
+        oauth_consumer_key: NuLE3hAVaJWmVMyRMnTM-A,
+        oauth_token: WzPzrA7tcIEv-YuRR1_ilXdyylqOdvVH,
+        oauth_signature_method: "HMAC-SHA1",
+        oauth_timestamp: new Date().getTime(),
+        oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+    })
+
+      var consumer_secret= adjPf1c0JJy2LWeAsqEz7QrL_sQ
+      var token_secret = 8gyS5zdHd4uaVCPiEoxZu_PjOU4
+      var signature = oauthSignature.generate(method, url, param, consumerSecret, tokenSecret, { encodeSignature: false});
+                        params['oauth_signature'] = signature;
+          //$http.jsonp(url, {params: params}).success(callback);
     }).then(function workingCallback(response){
       displayFoodStuff.data = response.data."whatever.whatever";
     }, function errorCallBack(response){
       console.log("Stop Yelpeing at me")
     });
-      return{
-        getContent: function(){
-          return displayFoodStuff.data;
-        }   
-      }
+      
 
 })
 
@@ -28,3 +39,9 @@ app.service('yelpService', function($http){
 
 
 
+
+
+
+
+
+           
