719883cfbf33839b2deb5efba99ac9516491e21a
Kris <krishermann@gmail.com>
2015-12-08 12:12:13 -0500

    Update missioncontrol.js

--- missioncontrol.js
+++ missioncontrol.js
@@ -1,8 +1,1 @@
-var app = angular.module("dcTable");
 
-app.controller('missionControl', function($scope, redditService) {
-
-		$scope./*API PAYLOAD*/ = redditService.getContent();
-		// getContent() returns response.data.children which is an array.
-
-});
