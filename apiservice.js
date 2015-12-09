92fa9a6061fad44395a74fa697133070cd27252c
Yasmine <yabd89@gmail.com>
2015-12-09 13:39:51 -0500

    servicesandcontrollersbyneighborhood

--- apiservice.js
+++ apiservice.js
@@ -6,16 +6,32 @@
 
 var app = angular.module("dcTable");
 
-app.controller('yelpity', ['$scope', 'myYelpAPI', function($scope, myYelpAPI) {
-    $scope.businesses = [];
-    $scope.addresses = [];
-    myYelpAPI.retrieveYelp('', function(data) {
-        $scope.businesses = data.businesses[11].name;
-        $scope.addresses  = data.businesses[11].location.address[0];
-        console.log(data);
-    });
+app.factory("myYelpAPI", function($http) {
+    return {
+        "retrieveYelp": function(name, callback) {
+            var method = 'GET';
+            var url = 'https://api.yelp.com/v2/search/?term=Restaurants&location=Detroit';
+            var params = {
+                    callback: 'angular.callbacks._0',
+                    location: 'Detroit',
+                    oauth_consumer_key: 'NuLE3hAVaJWmVMyRMnTM-A', //Consumer Key
+                    oauth_token: 'WzPzrA7tcIEv-YuRR1_ilXdyylqOdvVH', //Token
+                    oauth_signature_method: "HMAC-SHA1",
+                    oauth_timestamp: new Date().getTime(),
+                    oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+                    term: 'food'
+                };
+            var consumerSecret = 'adjPf1c0JJy2LWeAsqEz7QrL_sQ'; //Consumer Secret
+            var tokenSecret = '8gyS5zdHd4uaVCPiEoxZu_PjOU4'; //Token Secret
+            var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
+            params['oauth_signature'] = signature;
+            $http.jsonp(url, {params: params}).success(callback);
+        }
+    }
 
-}]).factory("myYelpAPI", function($http) {
+});
+
+app.factory("corktownAPI", function($http) {
     return {
         "retrieveYelp": function(name, callback) {
             var method = 'GET';
@@ -35,11 +51,82 @@ app.controller('yelpity', ['$scope', 'myYelpAPI', function($scope, myYelpAPI) {
             var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
             params['oauth_signature'] = signature;
             $http.jsonp(url, {params: params}).success(callback);
+          }
+      }
+
+  });
+
+  app.factory("midtownAPI", function($http) {
+      return {
+          "retrieveYelp": function(name, callback) {
+              var method = 'GET';
+              var url = 'https://api.yelp.com/v2/search/?term=Restaurants&location=Detroit, Midtown';
+              var params = {
+                      callback: 'angular.callbacks._0',
+                      location: 'Detroit',
+                      oauth_consumer_key: 'NuLE3hAVaJWmVMyRMnTM-A', //Consumer Key
+                      oauth_token: 'WzPzrA7tcIEv-YuRR1_ilXdyylqOdvVH', //Token
+                      oauth_signature_method: "HMAC-SHA1",
+                      oauth_timestamp: new Date().getTime(),
+                      oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+                      term: 'food'
+                  };
+              var consumerSecret = 'adjPf1c0JJy2LWeAsqEz7QrL_sQ'; //Consumer Secret
+              var tokenSecret = '8gyS5zdHd4uaVCPiEoxZu_PjOU4'; //Token Secret
+              var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
+              params['oauth_signature'] = signature;
+              $http.jsonp(url, {params: params}).success(callback);
+            }
         }
-    }
+
+    });
+
+app.factory("downtownAPI", function($http) {
+        return {
+            "retrieveYelp": function(name, callback) {
+                var method = 'GET';
+                var url = 'https://api.yelp.com/v2/search/?term=Restaurants&location=Detroit, Downtown';
+                var params = {
+                        callback: 'angular.callbacks._0',
+                        location: 'Detroit',
+                        oauth_consumer_key: 'NuLE3hAVaJWmVMyRMnTM-A', //Consumer Key
+                        oauth_token: 'WzPzrA7tcIEv-YuRR1_ilXdyylqOdvVH', //Token
+                        oauth_signature_method: "HMAC-SHA1",
+                        oauth_timestamp: new Date().getTime(),
+                        oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+                        term: 'food'
+                    };
+                var consumerSecret = 'adjPf1c0JJy2LWeAsqEz7QrL_sQ'; //Consumer Secret
+                var tokenSecret = '8gyS5zdHd4uaVCPiEoxZu_PjOU4'; //Token Secret
+                var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
+                params['oauth_signature'] = signature;
+                $http.jsonp(url, {params: params}).success(callback);
+              }
+          }
+
 });
-      
 
+      app.factory("estmktAPI", function($http) {
+          return {
+              "retrieveYelp": function(name, callback) {
+                  var method = 'GET';
+                  var url = 'https://api.yelp.com/v2/search/?term=Restaurants&location=Detroit, Eastern Market';
+                  var params = {
+                          callback: 'angular.callbacks._0',
+                          location: 'Detroit',
+                          oauth_consumer_key: 'NuLE3hAVaJWmVMyRMnTM-A', //Consumer Key
+                          oauth_token: 'WzPzrA7tcIEv-YuRR1_ilXdyylqOdvVH', //Token
+                          oauth_signature_method: "HMAC-SHA1",
+                          oauth_timestamp: new Date().getTime(),
+                          oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+                          term: 'food'
+                      };
+                  var consumerSecret = 'adjPf1c0JJy2LWeAsqEz7QrL_sQ'; //Consumer Secret
+                  var tokenSecret = '8gyS5zdHd4uaVCPiEoxZu_PjOU4'; //Token Secret
+                  var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
+                  params['oauth_signature'] = signature;
+                  $http.jsonp(url, {params: params}).success(callback);
+                }
+            }
 
-
- 
+});
--- myList.js
+++ myList.js
@@ -3,31 +3,63 @@ var app = angular.module('dcTable');
 app.controller('listController', function restaurantList($scope) {
 
    $scope.restaurants = [
-     {rest: 'Bucharest', neighborhood: 'Downtown'},
-     {rest: 'Ottava Via', neighborhood: 'Corktown'},
-     {rest: 'Supino Pizzeria', neighborhood: 'Eastern Market'}
+     {name: 'Bucharest', neighborhood: 'Downtown'},
+     {name: 'Ottava Via', neighborhood: 'Corktown'},
+     {name: 'Supino Pizzeria', neighborhood: 'Eastern Market'}
    ];
 
   $scope.add = function(name, neighborhood) {
      var newRestaurantAdd = {};
-     newRestaurantAdd.rest = $scope.newRestaurant;
+     newRestaurantAdd.name = $scope.newRestaurant;
      newRestaurantAdd.neighborhood= $scope.newNeighborhood;
      $scope.restaurants.push(newRestaurantAdd);
 
    }
 
 });
-app.controller('searchAPI', ['$scope', 'myYelpAPI', function($scope, myYelpAPI) {
-   
+app.controller('searchAPI', ['$scope', 'myYelpAPI', 'corktownAPI', 'midtownAPI', 'downtownAPI', 'estmktAPI', function($scope, myYelpAPI, corktownAPI, midtownAPI, downtownAPI, estmktAPI) {
+
    myYelpAPI.retrieveYelp('', function(data){
     $scope.apiList = [];
     for(var i = 0; i < data.businesses.length; i++){
           var stuff = data.businesses[i];
-          $scope.apiList.push(stuff.name);
-}
+          var newRestaurantAdd = {};
+          newRestaurantAdd.name = stuff.name;
+          newRestaurantAdd.neighborhood= stuff.neighborhood;
+          $scope.restaurants.push(newRestaurantAdd);
+        }
+    });
 
+    corktownAPI.retrieveYelp('', function(data){
+     $scope.apiList = [];
+     for(var i = 0; i < data.businesses.length; i++){
+           var stuff = data.businesses[i];
+           $scope.apiList.push(stuff.name);
+         }
+     });
 
+     midtownAPI.retrieveYelp('', function(data){
+      $scope.apiList = [];
+      for(var i = 0; i < data.businesses.length; i++){
+            var stuff = data.businesses[i];
+            $scope.apiList.push(stuff.name);
+          }
+      });
 
-   })
-       
+      downtownAPI.retrieveYelp('', function(data){
+       $scope.apiList = [];
+       for(var i = 0; i < data.businesses.length; i++){
+             var stuff = data.businesses[i];
+             $scope.apiList.push(stuff.name);
+           }
+       });
+
+       estmktAPI.retrieveYelp('', function(data){
+        $scope.apiList = [];
+        for(var i = 0; i < data.businesses.length; i++){
+              var stuff = data.businesses[i];
+              $scope.apiList.push(stuff.name);
+            }
+        });
+
   }])
