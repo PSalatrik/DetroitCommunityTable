9b06a4c1f3cf3fa6a51d87800916c1029dd9beb8
Kris Hermann <krishermann@gmail.com>
2015-12-10 13:08:59 -0500

    sortingliststopages

--- myList.js
+++ myList.js
@@ -2,11 +2,7 @@ var app = angular.module('dcTable');
 
 app.controller('listController', function restaurantList($scope) {
 
-   $scope.restaurants = [
-     {name: 'Bucharest', neighborhood: 'Downtown'},
-     {name: 'Ottava Via', neighborhood: 'Corktown'},
-     {name: 'Supino Pizzeria', neighborhood: 'Eastern Market'}
-   ];
+   $scope.restaurants = [];
 
   $scope.add = function(name, neighborhood) {
      var newRestaurantAdd = {};
@@ -17,15 +13,6 @@ app.controller('listController', function restaurantList($scope) {
 
       
   }
-
-  $scope.test = function (obj) {
-        if (obj === 'newRestaurant.name') {
-          console.log(true);
-        } 
-        else {
-          console.log("No restaurants match your search");
-        }
-   }
 
 });
 
@@ -50,7 +37,7 @@ app.controller('corktownCtrl', ['$scope', 'corktownAPI', function($scope, corkto
            var stuff = data.businesses[i];
            var newRestaurantAdd = {};
            newRestaurantAdd.name = stuff.name;
-           newRestaurantAdd.neighborhood= stuff.location.neighborhoods[0];
+           newRestaurantAdd.neighborhood= stuff.location.neighborhoods;
            $scope.restaurants.push(newRestaurantAdd);
            }
      });
