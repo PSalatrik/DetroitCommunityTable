383952f9c6d48d4e1c8c2bd25609c3231ce57401
Yasmine <yabd89@gmail.com>
2015-12-11 14:58:55 -0500

    estmkt WORKS TOOOOOOWOW

--- apiservice.js
+++ apiservice.js
@@ -157,13 +157,13 @@
      }
    }, {
      "getProp": function(name) {
-       return data.name;
+       return data;
 
      }
    }, {
      "setProp": function(name, value) {
 
-       data.name = value;
+       data.push(value);
      }
    }];
 
