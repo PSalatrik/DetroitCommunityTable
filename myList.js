cae06efdbcae8bbe6b20cadc9790aa73d0a08b31
Kris Hermann <krishermann@gmail.com>
2015-12-13 18:45:07 -0500

    to match yasmine

--- myList.js
+++ myList.js
@@ -1,11 +1,11 @@
 var app = angular.module('dcTable');
 
-app.controller('listController', ['$scope', 'corktownAPI', function($scope, corktownAPI) {
+app.controller('listController', ['$scope', 'corktownAPI', 'midtownAPI', 'downtownAPI', 'estmktAPI', function($scope, corktownAPI, midtownAPI, downtownAPI, estmktAPI) {
 
    $scope.pickedRestaurant = corktownAPI[1].getProp();
-   $scope.pickedRestaurant = midtownAPI[1].getProp();
-   $scope.pickedRestaurant = downtownAPI[1].getProp();
-   $scope.pickedRestaurant = estmktAPI[1].getProp();
+   //$scope.pickedRestaurant = midtownAPI[1].getProp();
+   //$scope.pickedRestaurant = downtownAPI[1].getProp();
+   //$scope.pickedRestaurant = estmktAPI[1].getProp();
 
 
    corktownAPI[0].retrieveYelp('', function(data){
