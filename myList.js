d37813658ce83b34fde87bf18a27fbcbf02e1453
Kris Hermann <krishermann@gmail.com>
2015-12-13 18:53:14 -0500

    more YA changes

--- myList.js
+++ myList.js
@@ -1,12 +1,11 @@
 var app = angular.module('dcTable');
 
-app.controller('listController', ['$scope', 'corktownAPI', 'midtownAPI', 'downtownAPI', 'estmktAPI', function($scope, corktownAPI, midtownAPI, downtownAPI, estmktAPI) {
+app.controller('listController', ['$scope', 'corktownAPI', function($scope, corktownAPI) {
 
    $scope.pickedRestaurant = corktownAPI[1].getProp();
-   //$scope.pickedRestaurant = midtownAPI[1].getProp();
-   //$scope.pickedRestaurant = downtownAPI[1].getProp();
-   //$scope.pickedRestaurant = estmktAPI[1].getProp();
-
+  //  $scope.pickedRestaurant = midtownAPI[1].getProp();
+  //  $scope.pickedRestaurant = downtownAPI[1].getProp();
+  //  $scope.pickedRestaurant = estmktAPI[1].getProp();
 
    corktownAPI[0].retrieveYelp('', function(data){
 
