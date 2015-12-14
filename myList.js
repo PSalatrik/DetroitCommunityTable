97dfeca7d236c51ca6f46754d459bb5ddda328b5
Yasmine <yabd89@gmail.com>
2015-12-14 16:57:48 -0500

    more hardcoding

--- myList.js
+++ myList.js
@@ -1,13 +1,10 @@
 var app = angular.module('dcTable');
 
-app.controller('listController', ['$scope', 'corktownAPI', 'midtownAPI', 'downtownAPI', 'estmktAPI', function($scope, corktownAPI, midtownAPI, downtownAPI, estmktAPI) {
+app.controller('listController', ['$scope', 'corktownAPI', function($scope, corktownAPI) {
 
   $scope.pickedRestaurant = corktownAPI[1].getProp();
-  $scope.pickedRestaurant = midtownAPI[1].getProp();
-  $scope.pickedRestaurant = downtownAPI[1].getProp();
-  $scope.pickedRestaurant = estmktAPI[1].getProp();
 
-   /*corktownAPI[0].retrieveYelp('', function(data){
+corktownAPI[0].retrieveYelp('', function(data){
 
           $scope.items = data.businesses;
           console.log($scope.items);
@@ -59,5 +56,5 @@ app.directive('typedir', function($timeout){
       };
     },
     templateUrl: 'searchTemplate.html'
-  });*/
+  });
 }]);
