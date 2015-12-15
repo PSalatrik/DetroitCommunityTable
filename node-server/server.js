300b275b1b93ebde586f689615fe5a7887a7afab
TimHeaphy <theheaphy@gmail.com>
2015-12-15 10:30:40 -0500

    documented and neverland

--- node-server/server.js
+++ node-server/server.js
@@ -1,28 +1,34 @@
 var path = require('path'),
-    fs = require('fs'),
+    // fs = require('fs'),
     express = require('express'),
+    app = express(),
     http = require('http'),
     yelp = require('./yelp'),
     crypto = require('crypto'),
     bodyParser = require('body-parser'),
-    app = express(),
     storage = require('node-persist'),
     http_port = process.env.HTTP_PORT || 8080,
     http_server = http.createServer(app);
 
+//initializes, names, and types 'persist' JSON
 storage.initSync();
-storage.setItem('restaurants', []);
+storage.setItem('neverland', []);
 
+//tell express that only JSON can be parsed from req.body
 app.use(bodyParser.json());
+//tells node/express which files to use by declaring absolute path
 app.use(express.static(path.resolve(__dirname + '/../')));
 
+//serves index at any route(i think) --ASK kj about this
 app.get('/api/', function(req, res) {
     res.sendFile(path.resolve(__dirname + '/../index.html'));
 });
 
+//maps to the contents of our retrieveYelp function, returns the 
+//search terms to be delivered by the api service all over the app
 app.get('/api/yelp/search', function(req, res) {
     return yelp.search({
-            term: 'Restaurants',
+            term: 'Bars',
             location: req.query.location
         })
         .then(function(data) {
@@ -33,19 +39,20 @@ app.get('/api/yelp/search', function(req, res) {
         });
 });
 
+//maps to the add functionality so we can give
 app.post('/api/restaurants/add', function(req, res) {
-    var restaurants = storage.getItem('restaurants') || [];
-    restaurants.push({
-        id: crypto.randomBytes(20).toString('hex'),
+    var neverland = storage.getItem('neverland') || [];
+    neverland.push({
+        // id: crypto.randomBytes(20).toString('hex'),
         name: req.body.restaurant
     });
-    storage.setItem('restaurants', restaurants);
-    res.send(restaurants);
+    storage.setItem('neverland', neverland);
+    res.send(neverland);
 });
 
 app.get('/api/restaurants/saved', function(req, res) {
-    var restaurants = storage.getItem('restaurants') || [];
-    res.send(restaurants);
+    var neverland = storage.getItem('neverland') || [];
+    res.send(neverland);
 });
 
 http_server.listen(http_port);
